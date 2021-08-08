cd857b92db40eb4d8953853e7730827af1907535
Gittingthehubbing <81262964+Gittingthehubbing@users.noreply.github.com>
2021-08-08 14:25:45 +0100

    Started implementing non-linear boundary

--- Logistic_Regression_Julia/main.jl
+++ Logistic_Regression_Julia/main.jl
@@ -10,6 +10,31 @@ b= 1.0;
 function f(x,a,b)
     a * x[1] .+ x[2].*b;
 end
+
+m_c = Array{Float32}([0.5,0.5,-0.1])';
+
+function f_circle(x_c,m_c)
+    m_c[:,1] .*x_c[:,1].^2 .+ m_c[:,2] .* x_c[:,2].^2 .+ m_c[:,3]
+end
+
+x_c = ones(11,2);
+x_c[:,1] = range(0,1,length=11);
+x_c[:,2] = range(0,1,length=11);
+
+
+X = x_c[:,1]' .* ones(size(x_c,1));
+Y =  ones(size(x_c,1))' .* x_c[:,2];
+Z_c = ones(size(X));
+i = j = 1
+for i = 1:size(X,1)
+    for j = 1:size(X,2)
+        Z_c[i,j] = f_circle([X[i,j], Y[i,j]]',m_c)[1]
+    end
+end
+
+p = contour(x_c[:,1] ,x_c[:,2] ,Z_c,levels=[0])
+
+
 x = Array{Float32}(0:0.1:1);
 x = hcat(x,ones(length(x)));
 descLine = f(x,a,b);
@@ -23,14 +48,20 @@ radomPoints = rand(size(x,1));
 
 classes = copy(radomPoints);
 classes .= 0;
+classes_c = copy(classes)
 i=1;
 for i=1:size(radomPoints,1)
     yFunc = f(x[i,:],a,b)
+    x_c_temp = hcat(x[i,1],radomPoints[i])
+    y_c = f_circle(x_c_temp,m_c)
     if radomPoints[i] > yFunc
         classes[i]=1;
     end
-    
-    println(i,x[i,:],yFunc)
+    if y_c < 0
+        classes_c[i]=0
+    else
+        classes_c[i]=1
+    end
 end
 
 boolIdx = classes .==1;
@@ -69,3 +100,5 @@ prediction = round.(sig(x*theta))
 accuracy = sum(prediction.==classes)/size(classes,1)
 
 println("Accuracy for training data is $(accuracy)")
+
+
