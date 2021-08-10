b3fd788f3ba16a89003d1dcd2c8244b1a71f3a08
Gittingthehubbing <81262964+Gittingthehubbing@users.noreply.github.com>
2021-08-10 09:03:58 +0100

    Implemented poly features and grad descent for non-linear boundary

--- Logistic_Regression_Julia/main.jl
+++ Logistic_Regression_Julia/main.jl
@@ -11,7 +11,7 @@ function f(x,a,b)
     a * x[1] .+ x[2].*b;
 end
 
-m_c = Array{Float32}([0.5,0.5,-0.1])';
+m_c = Array{Float32}([0.5,0.5,-0.2])';
 
 function f_circle(x_c,m_c)
     m_c[:,1] .*x_c[:,1].^2 .+ m_c[:,2] .* x_c[:,2].^2 .+ m_c[:,3]
@@ -32,10 +32,7 @@ for i = 1:size(X,1)
     end
 end
 
-p = contour(x_c[:,1] ,x_c[:,2] ,Z_c,levels=[0])
-
-
-x = Array{Float32}(0:0.1:1);
+x = range(0,1,length=11);
 x = hcat(x,ones(length(x)));
 descLine = f(x,a,b);
 descLineY = zeros(0);
@@ -53,7 +50,7 @@ i=1;
 for i=1:size(radomPoints,1)
     yFunc = f(x[i,:],a,b)
     x_c_temp = hcat(x[i,1],radomPoints[i])
-    y_c = f_circle(x_c_temp,m_c)
+    y_c = f_circle(x_c_temp,m_c)[1]
     if radomPoints[i] > yFunc
         classes[i]=1;
     end
@@ -64,17 +61,36 @@ for i=1:size(radomPoints,1)
     end
 end
 
+boolIdx_c = classes_c .==1;
+
+p = contour(x_c[:,1] ,x_c[:,2] ,Z_c,levels=[0])
+plot!(x[boolIdx_c,1],radomPoints[boolIdx_c],seriestype=:scatter,show=true)
+plot!(x[.!boolIdx_c,1],radomPoints[.!boolIdx_c],seriestype=:scatter,show=true)
+
+
 boolIdx = classes .==1;
 plot(x[:,1],descLineY,show=true)
 plot!(x[boolIdx,1],radomPoints[boolIdx],seriestype=:scatter,show=true)
 plot!(x[.!boolIdx,1],radomPoints[.!boolIdx],seriestype=:scatter,show=true)
+
+# create polynomial features for non-linear descLine
+
+deg = 3
+x_poly = hcat(ones(size(x_c,1)),copy(x_c))
+num = 1
+for d = 1:deg
+    for v = 0:d
+        x_poly=hcat(x_poly, x_c[:,1] .^(d-v) .*x_c[:,2] .^v)
+        num+=1
+    end
+end
+
 
 function sig(yPred)
     1 ./ (1 .+exp.(-1*yPred));
 end
 
 function cost(x,y,theta)
-    m = size(x,1);
     mean(-y .* log.(sig(x*theta)) .-(1 .-y) .*log.(1 .-sig(x*theta)));
 end
 
@@ -101,4 +117,12 @@ accuracy = sum(prediction.==classes)/size(classes,1)
 
 println("Accuracy for training data is $(accuracy)")
 
+theta_c =rand(size(x_poly,2))
+losses_c = ones(0)
+for e in range(1,stop=epochs,step=1)
+    append!(losses_c,cost(x_poly,classes_c,theta_c))
+    theta_c .-= lr .* grad(x_poly,classes_c,theta_c)
+end
 
+
+plot(range(1,stop=epochs,step=1),losses_c,show=true)
