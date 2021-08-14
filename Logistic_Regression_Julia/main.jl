12490dd29a47c7e5c1f22648ea4430508b77bbad
Gittingthehubbing <81262964+Gittingthehubbing@users.noreply.github.com>
2021-08-14 09:08:20 +0100

    Added regularization

--- Logistic_Regression_Julia/main.jl
+++ Logistic_Regression_Julia/main.jl
@@ -3,24 +3,50 @@ using DelimitedFiles
 using Plots
 using Statistics
 
-a = -1;
-b= 1.0;
-
 # create some data to classify
 function f(x,a,b)
-    a * x[1] .+ x[2].*b;
+    a .* x[:,1] .+ x[:,2].*b;
 end
-
-m_c = Array{Float32}([0.5,0.5,-0.2])';
 
 function f_circle(x_c,m_c)
     m_c[:,1] .*x_c[:,1].^2 .+ m_c[:,2] .* x_c[:,2].^2 .+ m_c[:,3]
 end
 
-x_c = ones(11,2);
-x_c[:,1] = range(0,1,length=11);
-x_c[:,2] = range(0,1,length=11);
+function sig(yPred)
+    1 ./ (1 .+exp.(-1*yPred));
+end
 
+function cost(x,y,theta)
+    mean(-y .* log.(sig(x*theta)) .-(1 .-y) .*log.(1 .-sig(x*theta)));
+end
+
+function grad(x,y,theta)
+    1/size(y,1) .*(x' *(sig(x*theta) .- y));
+end
+
+function cost_grad_reg(x,y,theta,lambda)
+    cost_reg = mean(-y .* log.(sig(x*theta)) .- (1 .- y) .*log.(1 .- sig(x*theta))) + lambda ./(2*size(x,1)) * sum(theta[2:end].^2);
+    # grad for bias term
+    grad1 = mean((sig(x*theta) .- y) .* x[:,1],dims=1)
+    #grad for rest of theta
+    grad_rest = mean((sig(x*theta) .- y) .* x[:,2:end],dims=1)' .+ lambda / size(x,1) .* theta[2:end]
+    grad_full = vcat(grad1,grad_rest)
+    return (cost_reg, grad_full)
+end
+
+
+a = -1;
+b= 0.2;
+
+num_data_points = 51;
+points_min = -0.5
+points_max = 1
+
+m_c = Array{Float32}([0.3,0.5,-0.15])';
+
+x_c = ones(num_data_points,2);
+x_c[:,1] = range(points_min,points_max,length=num_data_points);
+x_c[:,2] = range(points_min,points_max,length=num_data_points);
 
 X = x_c[:,1]' .* ones(size(x_c,1));
 Y =  ones(size(x_c,1))' .* x_c[:,2];
@@ -32,26 +58,29 @@ for i = 1:size(X,1)
     end
 end
 
-x = range(0,1,length=11);
+x = range(points_min,points_max,length=num_data_points);
 x = hcat(x,ones(length(x)));
 descLine = f(x,a,b);
 descLineY = zeros(0);
 
 for i = 1:size(x,1)
-    append!(descLineY,f(x[i,:],a,b))
+    append!(descLineY,f(x[i,:]',a,b))
 end
 
-radomPoints = rand(size(x,1));
+randomPoints = rand(size(x,1));
+randomPoints .-= minimum(randomPoints);
+randomPoints ./= maximum(randomPoints)
+randomPoints = (randomPoints .+ points_min) .* points_max;
 
-classes = copy(radomPoints);
+classes = copy(randomPoints);
 classes .= 0;
-classes_c = copy(classes)
+classes_c = copy(classes);
 i=1;
-for i=1:size(radomPoints,1)
-    yFunc = f(x[i,:],a,b)
-    x_c_temp = hcat(x[i,1],radomPoints[i])
+for i=1:size(randomPoints,1)
+    yFunc = f(x[i,:]',a,b)[1]
+    x_c_temp = hcat(x[i,1],randomPoints[i])
     y_c = f_circle(x_c_temp,m_c)[1]
-    if radomPoints[i] > yFunc
+    if randomPoints[i] > yFunc
         classes[i]=1;
     end
     if y_c < 0
@@ -63,15 +92,15 @@ end
 
 boolIdx_c = classes_c .==1;
 
-p = contour(x_c[:,1] ,x_c[:,2] ,Z_c,levels=[0])
-plot!(x[boolIdx_c,1],radomPoints[boolIdx_c],seriestype=:scatter,show=true)
-plot!(x[.!boolIdx_c,1],radomPoints[.!boolIdx_c],seriestype=:scatter,show=true)
+p = contour(x_c[:,1] ,x_c[:,2] ,Z_c,levels=[0]) # second axis showing is colorbar
+plot!(x[boolIdx_c,1],randomPoints[boolIdx_c],seriestype=:scatter,show=true)
+plot!(x[.!boolIdx_c,1],randomPoints[.!boolIdx_c],seriestype=:scatter,show=true)
 
 
 boolIdx = classes .==1;
 plot(x[:,1],descLineY,show=true)
-plot!(x[boolIdx,1],radomPoints[boolIdx],seriestype=:scatter,show=true)
-plot!(x[.!boolIdx,1],radomPoints[.!boolIdx],seriestype=:scatter,show=true)
+plot!(x[boolIdx,1],randomPoints[boolIdx],seriestype=:scatter,show=true)
+plot!(x[.!boolIdx,1],randomPoints[.!boolIdx],seriestype=:scatter,show=true)
 
 # create polynomial features for non-linear descLine
 
@@ -86,19 +115,8 @@ for d = 1:deg
 end
 
 
-function sig(yPred)
-    1 ./ (1 .+exp.(-1*yPred));
-end
-
-function cost(x,y,theta)
-    mean(-y .* log.(sig(x*theta)) .-(1 .-y) .*log.(1 .-sig(x*theta)));
-end
-
-function grad(x,y,theta)
-    1/size(y,1) .*(x' *(sig(x*theta) .- y));
-end
-
 theta = rand(2);
+
 
 epochs = 1000;
 lr=1e-1;
@@ -110,19 +128,27 @@ for e in range(1,stop=epochs,step=1)
     theta = theta .- lr .* grads 
 end
 
-plot(range(1,stop=epochs,step=1),losses,show=true)
+plot(range(1,stop=epochs,step=1),losses,show=false)
 
-prediction = round.(sig(x*theta))
-accuracy = sum(prediction.==classes)/size(classes,1)
+prediction = round.(sig(x*theta));
+accuracy = sum(prediction.==classes)/size(classes,1);
 
-println("Accuracy for training data is $(accuracy)")
+println("Accuracy for linear training data is $(accuracy)")
 
-theta_c =rand(size(x_poly,2))
-losses_c = ones(0)
+theta_c =rand(size(x_poly,2));
+losses_c = ones(0);
+lambda = 1e-2; # regularization term
+
 for e in range(1,stop=epochs,step=1)
-    append!(losses_c,cost(x_poly,classes_c,theta_c))
-    theta_c .-= lr .* grad(x_poly,classes_c,theta_c)
+    (c,g) = cost_grad_reg(x_poly,classes_c,theta_c,lambda);
+    append!(losses_c,c);
+    theta_c .-= lr .* g[:];
 end
 
 
 plot(range(1,stop=epochs,step=1),losses_c,show=true)
+
+prediction_c = round.(sig(x_poly*theta_c));
+accuracy_c = sum(prediction_c.==classes_c)/size(classes_c,1);
+
+println("Accuracy for circular training data is $(accuracy_c)")
